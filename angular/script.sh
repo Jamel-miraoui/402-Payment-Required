91c5f949aeae8c036f140cf5cc89fee446d6198e
jamel <jamel.miraoui.sl@gmail.com>
2022-12-19 15:34:43 +0100

    add angular and php folders

---
+++ angular/script.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+npm install -g @angular/cli 
+ng new angularshop //Create new Angular Project
+cd angularehop // Go inside the Angular Project Folder
+npm i bootstrap@4.3.1
+npm i popper.js
+npm i jquery
+npm install ngx-slick-carousel --save
+npm install slick-carousel --save
+ng g c header 
+ng g c footer
+ng g c home 
+ng g c productsingle
+ng g c cart
+ng g c checkout
+ng g c shop
+ng g c dashboard
+ng g c orders 
+ng g c login 
+ng g c signup
+ng g c forgot-password
+ng g c profile-details
+ng g c address
+ng g c edit-address
+
